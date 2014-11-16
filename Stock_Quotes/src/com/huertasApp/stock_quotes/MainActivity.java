fb6aa00ce3302b66b70aba9e0bd00d0c21344867
Juan Huertas <juanitohf@gmail.com>
2014-11-16 18:19:24 -0500

    This execute the thread each 10 second

--- Stock_Quotes/src/com/huertasApp/stock_quotes/MainActivity.java
+++ Stock_Quotes/src/com/huertasApp/stock_quotes/MainActivity.java
@@ -16,7 +16,6 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,7 +33,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	static InputStream is = null;
 	static JSONObject jObj = null;
 	static String json = "";
-	  
+	int seconds =0;  
 	EditText stockSymbol,CompayName,price; 
 	LinearLayout ll;
 	Button search;
@@ -74,7 +73,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	    	  		alertDialog("Company Quote are required");
 	    	  	}else{
 	    	  		/* If str Symbol are not empty make request with URl */
-	    	  		 new RetrieveMessages().execute(URL);
+	    	  		UpdateStock(URL); 
 	    	  	}
 	    	  	
 
@@ -124,72 +123,134 @@ public class MainActivity extends Activity implements OnClickListener {
 	 
 	 ////////  HERE START MY FUNCTION TO INTERECT WITH THE SERVER ////////
 	 ////////  THE INFORMATION WILL BE SENDING BY JSON FORMAT ///////////
-	 
-	
-	
-	 private class RetrieveMessages extends AsyncTask<String, Void, String> {
-	        protected String doInBackground(String... urls) {
-	            HttpClient client = new DefaultHttpClient();
-	            String json = "";
-	            try {
-	                String line = "";
-	                HttpGet request = new HttpGet(urls[0]);
-	                HttpResponse response = client.execute(request);
-	                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-	                while ((line = rd.readLine()) != null) {
-	                    json += line + System.getProperty("line.separator");
-	                }
-	            } catch (IllegalArgumentException e1) {
-	                e1.printStackTrace();
-	            } catch (IOException e2) {
-	                e2.printStackTrace();
-	            }
-	            
-	            Log.e("Json:",json);
-	            return json;
-	        }
 
-	        protected void onProgressUpdate(Void... progress) {
-
-	        }
-
-	        protected void onPostExecute(String result) {
-	          //  ll= (LinearLayout) findViewById(R.id.display);
-	        	
-	        	
-	        	  JSONObject reader = null;
-				try {
-					reader = new JSONObject(result);
-					
-                  JSONObject list  = reader.getJSONObject("list");
-                  JSONArray resources = list.getJSONArray("resources");
-                  
-                  JSONObject resource  = resources.getJSONObject(0);
-                  JSONObject fields  = resource.getJSONObject("resource").getJSONObject("fields");
-                  
-             
-                 String ComName = fields.getString("name");
-                 String priceCompany = fields.getString("price");
-                  
-                //  CompayName.setText(CompanyName);
-                 CompayName.setText("Company name: " + ComName);
-                 price.setText("Company price: " + priceCompany);
-					
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-                  
-	        	
-	        }
-	    }
 	
-	
-	
-	 
-	 
-
+	 protected void UpdateStock(final String URL){
 	       	
+		
+		Log.e("Result URL",URL);
+		 	Thread t = new Thread() {
+		 		
+	            public void run() {
+	            
+	            	while(true){
+	            		
+	            		try{
+			            	Thread.sleep(10000);
+			            	
+			            	
+			                HttpClient client = new DefaultHttpClient();
+				            String json = "";
+		                
+	           
+			                try {
+			                	
+			                	String line = "";
+			  	                HttpGet request = new HttpGet(URL);
+			  	                HttpResponse response = client.execute(request);
+			  	                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			  	                while ((line = rd.readLine()) != null) {
+			  	                    json += line + System.getProperty("line.separator");
+			  	                }
+			  	                
+			  	                
+			  	           
+
+			  	              updateView(json.toString());
+			  	           
+		
+			                } catch(Exception e) {
+			                    e.printStackTrace();
+			                 // createDialog("Error", "Cannot Estabilish Connection");
+			                }
+			              
+			                
+			                
+			                
+	            		} catch(Exception e) {
+		                    e.printStackTrace();
+		                 // createDialog("Error", "Cannot Estabilish Connection");
+		                }
+	            		
+	            	} // End while loop
+	               
+	            }
+	        };
+
+	        t.start();
+			
+
+	    }
+	 
+	 
+	 
+	 
+	 
+	 
+	 public static String slurp(final InputStream is) throws IOException
+	    {
+	    	 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	    	    StringBuilder out = new StringBuilder();
+	    	    String newLine = System.getProperty("line.separator");
+	    	    String line;
+	    	    while ((line = reader.readLine()) != null) {
+	    	        out.append(line);
+	    	        out.append(newLine);
+	    	    }
+	    	    return out.toString();
+	    }
+	 
+	 
+
+	 
+	 
+	 
+	 private void updateView(final String result) {
+		    runOnUiThread(new Runnable(){
+		        public void run() {
+		           
+
+					 JSONObject reader = null;
+						try {
+						   
+							reader = new JSONObject(result);
+							
+			               JSONObject list  = reader.getJSONObject("list");
+			               JSONArray resources = list.getJSONArray("resources");
+			               
+			               JSONObject resource  = resources.getJSONObject(0);
+			               JSONObject fields  = resource.getJSONObject("resource").getJSONObject("fields");
+			               
+			          
+			              String ComName = fields.getString("name");
+			              String priceCompany = fields.getString("price");
+			               
+			              
+			              /// Create error for debugin purpose
+			              seconds += 10;
+			              
+			              Log.e("Time",Integer.toString(seconds));
+			              Log.e("Company Name",ComName);
+			              Log.e("Price Company",priceCompany);
+			              
+			              
+			               CompayName.setText("Company name: " + ComName);
+			               price.setText("Company price: " + priceCompany);
+							
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					 
+		        	
+		        	
+		        	
+		        }
+		    });
+		}
+
+
+	
 		 
 	
 }// End main class
